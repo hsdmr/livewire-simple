622937740a32c276f7729f487dd8f95cb242d58c
hsdmr <hsdmrsoft@gmail.com>
2021-03-21 19:50:18 +0300

    update favicon

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kategoriler</title>
+    <link rel="icon" type="image/png" href="{{asset('logo.png')}}">
     <link rel="stylesheet" href="{{asset('css')}}/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('css')}}/all.min.css">
     <link rel="stylesheet" href="{{asset('css')}}/style.css">
